0d8188dad48c94bfb9f5cfe9ec17238558c73cae
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-09-18 08:57:35 -0400

    Installed plugins and dependencies, removed gatsby specific content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter Vincent`,
+    description: `This bare bones starter comes pre configured with MDX and styled components`,
+    author: `@vramdhanie`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,30 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
+    "gatsby-plugin-transition-link",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        plugins: [`gatsby-remark-images`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
